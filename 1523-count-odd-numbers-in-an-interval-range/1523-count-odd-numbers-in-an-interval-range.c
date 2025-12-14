6d839a00f46c1b9a7630ae8f0883e51c634cb1e1
Isiah Alinab <isiahalinab@gmail.com>
2025-12-14 11:49:10 +0800

    Time: 1792 ms (7.79%), Space: 7.7 MB (90.98%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.c
@@ -0,0 +1,11 @@
+
+
+int countOdds(int low, int high){
+    int count = 0;
+    for(int i = low; i <= high;i++){
+        if(i % 2 != 0){
+            count++;
+        }
+    }
+    return count;
+}
