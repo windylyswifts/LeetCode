4fcbec3554ea0b7d313c5b0a75ca7af1075189f8
Isiah Alinab <isiahalinab@gmail.com>
2026-07-07 18:49:08 +0800

    Time: 36 ms (86.57%), Space: 53.5 MB (51.14%) - LeetHub

--- 2667-create-hello-world-function/2667-create-hello-world-function.js
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -2,7 +2,8 @@
  * @return {Function}
  */
 var createHelloWorld = function() {
-    
+    //this problem shows how much we understand hgiher-order functions
+    //basically being able to return functions and set functions as parameter
     return function(...args) {
         return "Hello World";
     }
